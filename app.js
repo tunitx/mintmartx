4ecb407c383aea728d1f911c72532449b56811b1
tanish <imta819@gmail.com>
2023-08-02 20:00:48 +0530

    configured mongo string

--- app.js
+++ app.js
@@ -33,18 +33,45 @@ app.use(cookieParser(process.env.COOKIE_PARSER_SEC));
 const mongoSessionStore = require("connect-mongo");
 
 //? creating mongo db named mintMart
-async function connectToDatabase() {
+// async function connectToDatabase() {
+//   try {
+//     await mongoose.connect("mongodb+srv://tunitx:<FnPe7JctlVTlhJOT>@mintmart.wjhqljx.mongodb.net/?retryWrites=true&w=majority", {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Connected to MongoDB successfully!");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error.message);
+//   }
+// }
+// connectToDatabase();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://tunitx:FnPe7JctlVTlhJOT@mintmart.wjhqljx.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
   try {
-    await mongoose.connect("mongodb+srv://tunitx:<FnPe7JctlVTlhJOT>@mintmart.wjhqljx.mongodb.net/?retryWrites=true&w=majority", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB successfully!");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
-connectToDatabase();
+run().catch(console.dir);
+
 
 // !! Function to verify the webhook signature
 
@@ -140,7 +167,7 @@ require("./src/config/passport");
 
 const sessionStore = mongoSessionStore.create({
   collectionName: "sessions",
-  mongoUrl: "mongodb+srv://tunitx:<FnPe7JctlVTlhJOT>@mintmart.wjhqljx.mongodb.net/?retryWrites=true&w=majority",
+  mongoUrl: uri,
 });
 app.use(
   session({
